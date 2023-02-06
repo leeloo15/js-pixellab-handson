9410ccc3e27a65032d5fcee505dc44b5d6d3d528
Leeloo <leila150788@gmail.com>
2023-02-06 23:19:54 +0200

    Tema 20 dec

--- m01s05/ex11/app.js
+++ m01s05/ex11/app.js
@@ -2,10 +2,57 @@
 // Daca primeste un parametru, sa calculeze suprafata unui patrat. Daca primeste doi, a unui dreptunghi.
 // Daca primeste trei, sa calculeze suprafata totala a paralelipipedului. Foloseste structura switch.`)
 
-//console.warn(`2. Creeaza o functie numita calculateCircleArea() si afiseaza suprafata unor cercuri cu raza de 2, 20 si 16.`)
+function calculateSurface(width, length, height) {
+  switch (arguments.length) {
+    case 1:
+      return width * width;
+    case 2:
+      return width * length;
+    case 3:
+      return 2 * width * length + 2 * width * height + 2 * length * height;
+  }
+}
 
-//console.warn(`3 Creeaza un obiect numit pet cu urmatoarele metode: getName, getSpecies si getAge.`)
+console.warn(
+  `2. Creeaza o functie numita calculateCircleArea() si afiseaza suprafata unor cercuri cu raza de 2, 20 si 16.`,
+);
 
-//console.warn(`4. Folosind metodele obiectului pet afiseaza propozitia: “nnnn este ssss si are aaaa ani.”`)
+function calculateCircleArea(r) {
+  return Math.PI * r ** 2;
+}
+console.log(calculateCircleArea(2));
+console.log(calculateCircleArea(20));
+console.log(calculateCircleArea(16));
 
-//console.warn(`5. Folosind metoda getAge calculeaza si salveaza intr-o variabila numita petAge varsta animalului si afiseaza propozitia: “Animalul meu are petAge ani”`)
+console.warn(
+  `3 Creeaza un obiect numit pet cu urmatoarele metode: getName, getSpecies si getAge.`,
+);
+
+const pet = {
+  name: 'Aurora',
+  species: 'pisica',
+  age: 3,
+  getName: function () {
+    return this.name;
+  },
+  getSpecies: function () {
+    return this.species;
+  },
+  getAge: function () {
+    return this.age;
+  },
+};
+
+console.warn(
+  `4. Folosind metodele obiectului pet afiseaza propozitia: “nnnn este ssss si are aaaa ani.”`,
+);
+console.log(
+  `${pet.getName()} este ${pet.getSpecies()} si are ${pet.getAge()} ani.`,
+);
+
+console.warn(`5. Folosind metoda getAge calculeaza si salveaza intr-o variabila numita petAge varsta animalului si afiseaza propozitia:
+“Animalul meu are petAge ani.”`);
+
+const petAge = pet.getAge();
+
+console.log(`Animalul meu are ${petAge} ani.`);
